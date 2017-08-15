2b94aff1c7955b85f42b0fbf4fc5bf8d1cd77f64
Kevin Smith <k-p-smith@ieee.org>
2017-08-14 21:31:58 -0500

    enhance sql cmd line to return errors. and run time.

--- BlockchainToSql/Program.cs
+++ BlockchainToSql/Program.cs
@@ -1,16 +1,36 @@
-﻿using System;
-using System.IO;
-
-namespace BlockchainToSql
+﻿namespace BlockchainToSql
 {
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
     internal class Program
     {
         private static void Main(string[] args)
         {
-            var blocksFolder = Environment.ExpandEnvironmentVariables(@"%AppData%\Bitcoin\blocks");
-            var filesPath = Directory.GetFiles(blocksFolder, "blk*.dat", SearchOption.TopDirectoryOnly);
-            var parser = new BlockchainProcessor();
-            parser.Parse(filesPath);
+            var stopWatch = new Stopwatch();
+
+            try
+            {
+                var blocksFolder = Environment.ExpandEnvironmentVariables(@"%AppData%\Bitcoin\blocks");
+                var filesPath = Directory.GetFiles(blocksFolder, "blk*.dat", SearchOption.TopDirectoryOnly);
+                var parser = new BlockchainProcessor();
+
+                stopWatch.Start();
+                parser.Parse(filesPath);
+                stopWatch.Stop();
+
+                Console.WriteLine($"Done\nBlockchain parser ran for {stopWatch.ElapsedMilliseconds} ms.");
+            }
+            catch(Exception ex)
+            {
+                if (stopWatch.IsRunning)
+                    stopWatch.Stop();
+
+                Console.WriteLine($"Blockchain parse ran for {stopWatch.ElapsedMilliseconds} ms before erroring.\n{ex.Message}\n{ex.StackTrace}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
